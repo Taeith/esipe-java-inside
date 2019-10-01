f568d2328845d82378ec43beccd0503972a203d5
Taeith <nils.pernet@gmail.com>
2019-10-01 09:29:39 +0200

    Ajout Lab2

---
+++ lab2/src/test/java/fr/umv/java/inside/SwitchExampleTest.java
@@ -0,0 +1,38 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SwitchExampleTest {
+
+	@Test
+	void testDog() {
+		assertEquals(1, SwitchExample.switchExample("dog"));
+	}
+
+	@Test
+	void testCat() {
+		assertEquals(2, SwitchExample.switchExample("cat"));
+	}
+
+	@Test
+	void testHorse() {
+		assertEquals(4, SwitchExample.switchExample("horse"));
+	}
+
+	@Test
+	void testDog2() {
+		assertEquals(1, SwitchExample.switchExample2("dog"));
+	}
+
+	@Test
+	void testCat2() {
+		assertEquals(2, SwitchExample.switchExample2("cat"));
+	}
+
+	@Test
+	void testHorse2() {
+		assertEquals(4, SwitchExample.switchExample2("horse"));
+	}
+
+}
+
